89805576fb1bb81215f7f5a3cfa11b8caaf092b1
CozyBrian <dragonbrian33@gmail.com>
2024-06-15 01:45:35 +0000

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Home from './src/screens/Home';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    <GestureHandlerRootView>
+      <SafeAreaView style={styles.container}>
+      <Home />
+      </SafeAreaView>
+    </GestureHandlerRootView>
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
